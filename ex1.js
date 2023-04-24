0a237c6802487635074521c57d44c1f1927083a7
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-04-24 21:14:27 +0300

    binar operator

--- ex1.js
+++ ex1.js
@@ -1,3 +1,11 @@
-let x = 2.8,
+/*let x = 2.8,
   y = 7.3;
-console.log(x - y);
+console.log(x - y);*/
+
+let x = "2.8",
+  y = 7.3;
+console.log(x - y); // c бинарным оператор минус строка автоматически посчиталась числом
+
+let z = -x;
+y = 7.3;
+console.log(z, typeof z);
