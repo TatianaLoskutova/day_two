8e340bfe6e8c8b3e906edaefdd4292cad3604f13
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-04-24 21:41:29 +0300

    unar summ

--- ex2.js
+++ ex2.js
@@ -1,4 +1,21 @@
-let x = 4,
+/*let x = 4,
   y = 7;
 let sum = x + y;
-console.log(sum);
+console.log(sum);*/
+
+let x = "4",
+  y = 7;
+let sum = x + y;
+console.log(sum); // тут уже строка получается
+
+let a = "4",
+  b = 7;
+let sumUnar = +a; // унарный оператор прерващает строку в число
+console.log(sumUnar, typeof sumUnar);
+
+let val = "56";
+let dig = +val;
+
+let c = "4",
+  d = 5;
+console.log(+c + +d); // вот такой хитровыделенный спосокб  сделать число при помощи унарного оператора
